094b8703c1e07e39c885ad18c35ed44445b58f04
gravindrareddy <raviguntaka.reddy@gmail.com>
2016-08-21 11:46:06 +0530

    initial commit

---
+++ app/src/main/java/smallbusiness/redgun/android/singlepageapp/SmallBusinessActivity.java
@@ -0,0 +1,13 @@
+package smallbusiness.redgun.android.singlepageapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmallBusinessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_small_business);
+    }
+}
